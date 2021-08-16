cda30f228ecf344c278b8982805efb4293e99bf6
rzhu1999 <rzhu08448@gmail.com>
2021-08-15 19:44:35 -0700

    Implement the REACT router, Activate two components as links: Register & Login

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,27 @@
 import React, { Fragment } from 'react'; // Fragment won't actually show up in the dom (ghost ele)
 
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+
 import './App.css';
 
 const App = () => (
-    <Fragment>
-        <Navbar />
-        <Landing />
-    </Fragment>
+    <Router>
+        <Fragment>
+            <Navbar />
+            <Route exact path='/' component={Landing} />
+            <section className='container'>
+                <Switch>
+                    <Route exact path='/register' component={Register} />
+                    <Route exact path='/login' component={Login} />
+                </Switch>
+            </section>
+        </Fragment>
+    </Router>
 );
 
 export default App;
