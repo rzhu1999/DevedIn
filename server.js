b11dd296623afcde490430fcad07fba416716f3d
rzhu1999 <rzhu08448@gmail.com>
2021-12-28 18:14:26 -0800

    Update gitignore

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -13,13 +14,23 @@ connectDB();
 app.use(express.json({ extended: false }));
 
 // Single endpoint just to test out
-app.get('/', (req, res) => res.send('API Running'));
+// app.get('/', (req, res) => res.send('API Running'));
 
 // Define Routes
 app.use('/api/user', require('./routes/api/user'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/post', require('./routes/api/post'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 // look for an environment variable called port to use.
 // when we deploy to Horoku, that's where it's going to get the port number
